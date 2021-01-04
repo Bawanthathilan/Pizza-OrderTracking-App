fc238a2e378bfe3fca8ac4c0c83bfc49f02c1316
bawanthathilan <bawantharathnayaka@gmail.com>
2021-01-04 14:44:17 +0530

    empty cart error fixed

--- app/http/controllers/customers/cartController.js
+++ app/http/controllers/customers/cartController.js
@@ -1,10 +1,13 @@
-function cartController(){
-    return{
-        index(req,res){
-            return res.render('customers/cart')
+const { json } = require("express")
+
+function cartController() {
+    return {
+        index(req, res) {
+            res.render('customers/cart')
         },
-        update(req,res){
-            //for the first time create cart and adding basic object structure
+        update(req, res) {
+            
+            // for the first time creating cart and adding basic object structure
             if (!req.session.cart) {
                 req.session.cart = {
                     items: {},
@@ -13,23 +16,22 @@ function cartController(){
                 }
             }
             let cart = req.session.cart
-            console.log(req.body)
-                //check item dows not exist in cart
-                if(!cart.items[req.body._id]) {
-                    cart.items[req.body._id] = {
-                        item: req.body,
-                        qty: 1
-                    }
-                    cart.totalQty = cart.totalQty + 1
-                    cart.totalPrice = cart.totalPrice + req.body.price
-                } else {
-                    cart.items[req.body._id].qty = cart.items[req.body._id].qty + 1
-                    cart.totalQty = cart.totalQty + 1
-                    cart.totalPrice =  cart.totalPrice + req.body.price
+
+            // Check if item does not exist in cart 
+            if(!cart.items[req.body._id]) {
+                cart.items[req.body._id] = {
+                    item: req.body,
+                    qty: 1
                 }
-                return res.json({ totalQty: req.session.cart.totalQty })
+                cart.totalQty = cart.totalQty + 1
+                cart.totalPrice = cart.totalPrice + req.body.price
+            } else {
+                cart.items[req.body._id].qty = cart.items[req.body._id].qty + 1
+                cart.totalQty = cart.totalQty + 1
+                cart.totalPrice =  cart.totalPrice + req.body.price
+            }
+            return res.json({ totalQty: req.session.cart.totalQty })
         }
-        
     }
 }
 
