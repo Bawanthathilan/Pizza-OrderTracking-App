ed7000696f88058a768c630dd05bf61c0a5a8a1f
bawanthathilan <bawantharathnayaka@gmail.com>
2021-01-04 12:40:31 +0530

    sessions config

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
@@ -5,6 +6,10 @@ const path = require('path');
 const expressLayout = require('express-ejs-layouts');
 const PORT = process.env.PORT || 3000
 const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('express-flash');
+const MongoDbStore = require('connect-mongo')(session)
+
 
 //DB Connection
 const url = "mongodb://localhost/pizza"
@@ -20,7 +25,22 @@ connection.once('open',()=>{
     console.log('Connection failed...')
 });
 
+//session store
+let mongoStore = new MongoDbStore({
+    mongooseConnection : connection,
+    collection: 'sessions'
+})
+//session config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave:false,
+    store: mongoStore,
+    saveUninitialized:false,
+    cookie: {maxAge: 1000 * 60 * 60 * 24}
+    
+}))
 
+app.use(flash());
 
 //assets
 app.use(express.static('public'))
