133ff1d5eb8cd6d8508cf9275c83f0f077f1d71b
bawanthathilan <bawantharathnayaka@gmail.com>
2021-01-04 18:44:40 +0530

    register and login , logout functions done :white_check_mark:

--- server.js
+++ server.js
@@ -9,14 +9,15 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const flash = require('express-flash');
 const MongoDbStore = require('connect-mongo')(session)
-
+const passport = require('passport');
 
 //DB Connection
 const url = "mongodb://localhost/pizza"
 mongoose.connect(url, {
     useNewUrlParser:true,
     useUnifiedTopology:true,
-    useFindAndModify:true
+    useFindAndModify:false,
+    useCreateIndex:true
 });
 const connection = mongoose.connection;
 connection.once('open',()=>{
@@ -24,6 +25,7 @@ connection.once('open',()=>{
 }).catch(err=>{
     console.log('Connection failed...')
 });
+
 
 //session store
 let mongoStore = new MongoDbStore({
@@ -40,6 +42,12 @@ app.use(session({
     
 }))
 
+//passport config
+const passportInit = require('./app/config/passport');
+passportInit(passport)
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash());
 
 //assets
@@ -50,6 +58,7 @@ app.use(express.json());
 //global middleware
 app.use((req , res , next)=>{
     res.locals.session = req.session
+    res.locals.user = req.user
     next()
 })
 //set Template engine
