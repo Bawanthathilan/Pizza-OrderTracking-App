efa912df569dc69e3a08dc19ef902cbad8b680e2
bawanthathilan <bawantharathnayaka@gmail.com>
2021-01-04 10:51:20 +0530

    login page with route done: 7&8 :white_check_mark:

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ app.get('/cart',(req,res)=>{
     res.render('customers/cart');
 });
 
+app.get('/login',(req,res)=>{
+    res.render('auth/login');
+});
+
+app.get('/register',(req,res)=>{
+    res.render('auth/register');
+});
+
 const PORT = process.env.PORT || 3000
 app.listen(3000, ()=>{
     console.log(`Listning on port ${PORT}`)
